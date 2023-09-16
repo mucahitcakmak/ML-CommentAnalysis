50b21cab954670ae9736ed1ed2486d70d7d64a58
Mücahit Çakmak <mucoox18@gmail.com>
2023-09-16 15:48:25 +0300

    Comment lines edited.

--- CommentAnalysis.py
+++ CommentAnalysis.py
@@ -1,74 +1,35 @@
 import pandas as pd
 import numpy as np
 
+# Data Import
+data = pd.read_csv("data/comments_and_reviews.csv")
 
-# Data Import **
-data = pd.read_csv("data/IMDB-Dataset.csv")
-
-
-# Bag Of Word | Kelime Dağarcığı ** 
-"""
-# Kütüphanesiz kendi ellerimizle yazdık
-
-# Preprocessing **
-observations = ""
-from bs4 import BeautifulSoup
-def removeHTML(text):
-    soup =  BeautifulSoup(text, "html.parser")
-    return soup.get_text()
-
-for n in range(20):
-    observations += removeHTML(data.iloc[n, 0])
-
-observationsClean = ",".join(i.lower() for i in observations.split() if i.isalnum())
-
-setOfWords = set(observationsClean.split(","))
-
-
-from tqdm import tqdm # döngünün ne kadar süreceğini gösteren çubuk
-
-dictList = []
-for n in tqdm(range(len(data))):
-    observation = removeHTML(data.iloc[n, 0])
-    clean = ",".join(i.lower() for i in observations.split() if i.isalnum())
-    dictOfWords = dict.fromkeys(setOfWords, 0)
-    for word in clean.split(","):
-        if word in dictOfWords:
-            dictOfWords[word] += 1
-    dictList.append(dictOfWords)
-"""
-# Kütüphane ile bow
-# Bizim kendi yerimize cümle içindeki html tagleri veya noktalama işaretlerini vs temizler
+# We organize our data set.
 from sklearn.feature_extraction.text import CountVectorizer
 y = data.sentiment.replace({"positive":1, "negative":0})
 x = data.review
-
 bag = CountVectorizer()
 X = bag.fit_transform(x)
-
 
 # Data Train
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 
-
-# Random Forest
+# Train testing data with Random Forest
 from sklearn.ensemble import RandomForestClassifier
 rfc = RandomForestClassifier(n_jobs=-1)
 rfc.fit(X_train, y_train)
 
-y_pred = rfc.predict(X_test)
-
-
 # Predict
+y_pred = rfc.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-# print(cm)
+# print(cm)  # -> Accuracy rate
 
 
-# Kendi yorumumu tahmin ettiricem
-test1 = "I really did not enjoy watching this. Very disappointed." # real = 0
-test2 = "What a wonderful movie. I enjoyed watching this with my kids." # real = 1
+# We are testing the algorithm
+test1 = "Very disappointed." # expected output = [0]
+test2 = "What a wonderful movie. I enjoyed watching this with my kids." # expected output = [1]
 
 print(rfc.predict(bag.transform([test1])))
 print(rfc.predict(bag.transform([test2])))
---
+++ README.md
@@ -0,0 +1,10 @@
+# Project Details
+
+**There are two headers in the dataset :**
+- Review: Comments made about the movie. 
+- Sentiment: Whether the comment made is positive or negative.
+
+![](_res/image1.png)
+
+We train this data set with the **random forest** algorithm. As input, it tells us whether the sentences we enter into the program are positive or negative. 
+**More details are in the code.**
